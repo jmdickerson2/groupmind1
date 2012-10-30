fe0727033a122ff9f785e0a7829efbd139be343c
James Dickerson <jmdickerson2@gmail.com>
2012-10-30 12:12:09 -0400

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupmind1::Application.config.secret_token = '36937d130b4f250389ffe6e08e5a41ba392f4468004599fb801be06422896c5c56aeff109413ee7522a360907a220f6d47928e852d0f89fbf1dfb021d02bd351'
